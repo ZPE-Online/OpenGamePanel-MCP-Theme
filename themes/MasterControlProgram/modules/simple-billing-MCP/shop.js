893f128c2faeb4d995c1934330f692c82c094d05
y4ahmad <yaseen.ahmad2009@gmail.com>
2021-06-21 13:11:06 -0400

    MCP Billing module added

---
+++ themes/MasterControlProgram/modules/simple-billing-MCP/shop.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.main [href="?m=simple-billing-MCP&p=cart"]').addClass('btn').addClass('btn-sm').addClass('btn-primary').prepend('<i class="fa fa-shopping-cart" aria-hidden="true"></i>');
+
+	if($('.custom-range-slider').length>0) {
+   $(".custom-range-slider").ionRangeSlider({});
+}
+
+});
+
+	
+	
